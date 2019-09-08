e5950956fd452179d3104909ed59ea103abbd332
shah <najam2012@hotmail.com>
2019-09-08 17:55:51 +0500

    implemented part 1

--- ss.py
+++ ss.py
@@ -9,31 +9,46 @@ tknzr = TweetTokenizer()
 
 all_files = glob.glob('corpus\*')
 
-out = open("outfile.txt", 'w', encoding='UTF-8')
-VALID_TAGS = ['div', 'span', 'p', 'center', 'b', 'strong', 'em', 'i', 'ol', 'ul', 'li', 'dl', 'dt', 'dd', 'table', 'td',
-              'tr', 'th', 'h1', 'h2', 'h3', 'h4', 'h5']
-
 stop_list = open('stoplist.txt', 'r').read()
 stop_list = tknzr.tokenize(stop_list)
+
+#termids=open("termids.txt",'w',encoding='UTF-8')
+#docids=open ("docids.txt",'w',encoding='UTF-8')
+
+#term_index=open('term_index.txt','w',encoding='UTF=8')
+
+VALID_TAGS = ['div', 'span', 'p', 'a','center', 'b', 'strong', 'em', 'i', 'ol', 'ul', 'li', 'dl', 'dt', 'dd', 'table', 'td',
+              'tr', 'th', 'h1', 'h2', 'h3', 'h4', 'h5']
+
 
 # Snowball stemmer is used for stemming
 stemmer = SnowballStemmer('english')
 
+termidhash={}
+count_term_ids=0
+count_doc_ids=0
+
 for filename in all_files:
-    filtered = []
-    a=[]
-    fileobject = open(filename, 'rb')
-    soup = BeautifulSoup(fileobject, 'html.parser')
-    #for script in soup("script"):
-     #   script.decompose()
-    for x in soup.find_all(VALID_TAGS):
-        a += tknzr.tokenize(x.get_text())
-    for word in a:
-        if word not in stop_list and re.match("^[a-zA-Z0-9]+((['][_][-][a-zA-Z0-9])?[a-zA-Z0-9]*)*$", word) and len(word)>1:
-            word = word.lower()
-            #word = stemmer.stem(word)
-            filtered.append(word)
-    if filtered:
-        out.write(str(filtered))
-    fileobject.close()
-out.close()
+    with open ("docids.txt",'a') as docids :
+        docids.write(str(str(count_doc_ids)+'\t'+filename+'\n'))
+    count_doc_ids=count_doc_ids+1
+    try:
+        with open(filename, 'rb') as fileobject :
+            soup = BeautifulSoup(fileobject, 'html.parser')
+            for x in soup.find_all(VALID_TAGS):
+                a=tknzr.tokenize(x.get_text())
+                for word in a:
+                    word = word.lower()
+                    if word not in stop_list and re.match("^[a-zA-Z0-9]+((['][_][-][a-zA-Z0-9])?[a-zA-Z0-9]*)*$", word) and len(word)>1:
+                        #word = stemmer.stem(word)
+                        if not word in termidhash:
+                            termidhash[word] = count_term_ids
+                            count_term_ids = count_term_ids + 1
+                            with open("termids.txt", 'a') as termids :
+                                termids.write(str(str(count_term_ids)+'\t'+ word +'\n'))
+    except IOError:
+        print(IOError)
+
+docids.close()
+termids.close()
+#term_index.close()
